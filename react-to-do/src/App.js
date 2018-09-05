02f359f915ffd9b8f9947866881f6c688ede41aa
bastienfp <bastien.fp@gmail.com>
2018-09-05 17:41:20 -0400

    Completed assignment React To-Do: Events

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -33,12 +33,19 @@ class App extends Component {
     this.setState({ todos: todos });
   }
 
+  deleteToDo(index) {
+    const todos = this.state.todos.slice();
+    const deletedToDo = todos[index];
+    const cleanedToDos = todos.filter( todo => todo !== deletedToDo );
+    this.setState({ todos: cleanedToDos});
+  }
+
   render() {
     return (
       <div className="App">
         <ul>
           { this.state.todos.map( (todo, index) =>
-            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) }/>
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index)} deleteToDo={ () => this.deleteToDo(index)}/>
           )}
         </ul>
         <form onSubmit={ (e) => this.handleSubmit(e) }>
